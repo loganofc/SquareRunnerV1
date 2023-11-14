84c3cbe037ecbf2eba0ef6825e98f18b5a439f2f
logan <141536342+loganofc@users.noreply.github.com>
2023-11-14 16:14:41 -0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+function getMessage() {
+  return "Welcome to cube slide by noisy boy enjoy v1 dont think this games ez :)";
+}
+(function (_0x39c2f3, _0x6523f) {
+  _0x39c2f3.onload = function () {
+    alert("Welcome to cube slide by noisy boy enjoy v1 dont think this games ez :)");
+  };
+})(window, document);
