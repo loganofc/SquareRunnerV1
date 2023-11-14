69a98360984f1b47d2276b0a250a79633d684763
logan <141536342+loganofc@users.noreply.github.com>
2023-11-14 16:20:34 -0600

    popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function getMessage() {
-  return "Welcome to cube slide by noisy boy enjoy v1 dont think this games ez :)";
+  return "Welcome to Square Runner skidded from Noisy Boy :)";
 }
 (function (_0x39c2f3, _0x6523f) {
   _0x39c2f3.onload = function () {
-    alert("Welcome to cube slide by noisy boy enjoy v1 dont think this games ez :)");
+    alert("Welcome to Square Runner skidded from Noisy Boy :)");
   };
 })(window, document);
